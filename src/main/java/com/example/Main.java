0f989d0046dbc521d6a9bc6eedbfcbf271d7f2df
snap-29 <90902819+snap-29@users.noreply.github.com>
2026-02-19 17:19:33 +0300

    Update Main.java

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+public class App{
+  public static int ymnojenie(int x, int y){
+    return x*y;
+  } public static void main(String[]args) {
+      Sustem.out.println(ymnojenie(6,7));
+  }
+}
