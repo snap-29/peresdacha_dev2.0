8883183d8a8ce33d837007625faaf6afed8e04f3
snap-29 <90902819+snap-29@users.noreply.github.com>
2026-02-19 17:24:26 +0300

    Update MainTest.java

---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,8 @@
+package com.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+class MainTest{
+  @Test
+  void ymnojenieTest
+    assertEquals(42, Main.add(6,7));
+}
